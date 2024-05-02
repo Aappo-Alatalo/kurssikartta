52f6dc73a3e14bbfd72a0fe02dbdd3dedb233f68
Aappo Alatalo <aappo.alatalo@gmail.com>
2024-05-02 22:54:49 +0300

    Add login/register functionality Code does not display error messages to the user in the current state.

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
-from flask import redirect, render_template, request
+from flask import redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
 from os import getenv
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
 
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
@@ -14,3 +16,52 @@ def index():
     sql = "SELECT name, credits, description FROM Courses"
     fetched_courses = db.session.execute(text(sql)).fetchall()
     return render_template("index.html", fetched_courses=fetched_courses)
+
+@app.route("/login",methods=["POST"])
+def login():
+    username = request.form["username"]
+    input_password = request.form["password"]
+    # Check user input
+    sql = "SELECT id, password FROM users WHERE username=:username"
+    result = db.session.execute(text(sql), {"username":username})
+    user = result.fetchone()
+    if not user:
+        # TODO: invalid username
+        return redirect("/")
+    else:
+        db_password = user.password
+        if check_password_hash(db_password, input_password):
+            # Correct username and password
+            session["username"] = username
+            return redirect("/")
+        else:
+            # TODO: invalid password
+            return redirect("/")
+        
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/create_user",methods=["POST"])
+def create_user():
+    username = request.form["username"]
+    password = request.form["password"]
+    # Check user input
+    if len(username) < 3:
+        # TODO: Show error message, username too short
+        return redirect("/")
+    elif len(password) < 5:
+        # TODO: Show error message, password too short
+        return redirect("/")
+    # Insert data into database
+    password_hash = generate_password_hash(password)
+    sql = "INSERT INTO users (username, password, account_type) VALUES (:username, :password, 0)"
+    db.session.execute(text(sql), {"username":username, "password":password_hash})
+    db.session.commit()
+    session["username"] = username
+    return redirect("/")
--- schema.sql
+++ schema.sql
@@ -1,11 +1,19 @@
-DROP TABLE IF EXISTS Courses CASCADE;
+DROP TABLE IF EXISTS courses CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 
-CREATE TABLE Courses (
+CREATE TABLE courses (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     credits INTEGER NOT NULL,
     description TEXT
 );
 
-INSERT INTO Courses (name, credits, description) VALUES ('Ohjelmoinnin perusteet', 5, 'Opiskelijan ensimmänen kosketus ohjelmointiin.');
-INSERT INTO Courses (name, credits, description) VALUES ('Ohjelmoinnin jatkokurssi', 5, 'Opiskelijan toinen kosketus ohjelmointiin.');
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    account_type INTEGER NOT NULL
+);
+
+INSERT INTO courses (name, credits, description) VALUES ('Ohjelmoinnin perusteet', 5, 'Opiskelijan ensimmänen kosketus ohjelmointiin.');
+INSERT INTO courses (name, credits, description) VALUES ('Ohjelmoinnin jatkokurssi', 5, 'Opiskelijan toinen kosketus ohjelmointiin.');
