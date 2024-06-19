749ecec89422942692a9ba133e867c05fd5bdef8
Aappo Alatalo <aappo.alatalo@gmail.com>
2024-06-20 00:32:52 +0300

    configure Fly.io
    This may destroy "flask run" usability.

--- db.py
+++ db.py
@@ -2,5 +2,6 @@ from app import app
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+#app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL") This for local
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1) # This for Fly.io
 db = SQLAlchemy(app)
